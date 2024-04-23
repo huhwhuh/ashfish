feadd31b1cc04440773ad5f480c7da3738ccb405
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2024-04-23 18:00:37 -0400

    Add login, profile, register, and forgot-password routes

---
+++ src/routes/forgot-password/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { Input } from '$lib/components/ui/input';
+	import { Button } from '$lib/components/ui/button';
+	import { Label } from '$lib/components/ui/label';
+	import * as Card from '$lib/components/ui/card';
+
+	export let data: PageData;
+	console.log(data);
+</script>
+
+<Card.Root class="mx-auto max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-2xl">Forgot Password</Card.Title>
+		<Card.Description>Enter your email below request a password reset</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<div class="grid gap-4">
+			<div class="grid gap-2">
+				<Label for="email">Email</Label>
+				<Input id="email" type="email" placeholder="email@example.com" required />
+			</div>
+			<Button type="submit" class="w-full">Login</Button>
+		</div>
+	</Card.Content>
+</Card.Root>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+
+	export let data: PageData;
+	console.log(data);
+</script>
+
+<Card.Root class="mx-auto max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-xl">Register</Card.Title>
+		<Card.Description>Enter your information to create an account</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<div class="grid gap-4">
+			<div class="grid gap-2">
+				<Label for="username">Username</Label>
+				<Input id="username" placeholder="Max" required />
+			</div>
+			<div class="grid gap-2">
+				<Label for="email">Email</Label>
+				<Input id="email" type="email" placeholder="email@example.com" required />
+			</div>
+			<div class="grid gap-2">
+				<Label for="password">Password</Label>
+				<Input id="password" type="password" />
+			</div>
+			<Button type="submit" class="w-full">Create an account</Button>
+		</div>
+		<div class="mt-4 text-center text-sm">
+			Already have an account?
+			<a href="/login" class="underline"> Log in </a>
+		</div>
+	</Card.Content>
+</Card.Root>
