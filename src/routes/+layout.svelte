bfa0bc00088207e295b7b555f5dd2baa47526605
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2024-04-23 17:59:30 -0400

    Remove boilerplate files and added Navbar
    The Sverdle game and its related files and routes, as well as the 'Counter' and 'About' components have been deleted.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Header from './Header.svelte';
+	import Navbar from './Navbar.svelte';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
 </script>
@@ -7,15 +7,11 @@
 <ModeWatcher />
 
 <div class="flex flex-col min-h-screen">
-	<Header />
+	<Navbar />
 
 	<main class="flex flex-1 flex-col p-4 w-full max-w-5xl m-auto box-border">
 		<slot />
 	</main>
 
-	<footer class="flex flex-col content-center items-center p-3 xs:py-3">
-		<p>
-			visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-		</p>
-	</footer>
+	<footer class="flex flex-col content-center items-center p-3 xs:py-3"></footer>
 </div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	// import { page } from '$app/stores';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import { Separator } from '$lib/components/ui/separator';
+
+	let isLoggedIn = true;
+</script>
+
+<header class="w-screen">
+	<span class="flex flex-row items-center pl-2 space-x-1">
+		<Button on:click={toggleMode} variant="ghost" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+		<Separator orientation="vertical" class="h-7" />
+		<nav class="min-h-10 items-center py-1">
+			<ul class="flex flex-row justify-end space-x-5">
+				{#if isLoggedIn}
+					<li>
+						<Button
+							on:click={() => {
+								isLoggedIn = false;
+							}}
+							variant="ghost">DEBUG_LOG_OUT</Button
+						>
+					</li>
+				{:else}
+					<li>
+						<Button
+							on:click={() => {
+								isLoggedIn = true;
+							}}
+							variant="ghost">DEBUG_LOG_IN</Button
+						>
+					</li>
+				{/if}
+				<li>
+					<Button href="/" variant="ghost">Home</Button>
+				</li>
+				<li>
+					<Button href="/" variant="ghost">Chat</Button>
+				</li>
+				{#if isLoggedIn}
+					<li>
+						<Button href="/login" variant="ghost" data-sveltekit-preload-data="hover"
+							>Profile</Button
+						>
+					</li>
+				{:else}
+					<li>
+						<Button href="/login" variant="ghost" data-sveltekit-preload-data="hover">Login</Button>
+					</li>
+				{/if}
+			</ul>
+		</nav>
+	</span>
+	<Separator />
+</header>
