77e697c1e3ba0791d38a8b292cc00c2054119966
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2024-04-24 14:39:13 -0400

    Update Navbar design

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	// import { page } from '$app/stores';
 	import Sun from 'lucide-svelte/icons/sun';
 	import Moon from 'lucide-svelte/icons/moon';
 	import { toggleMode } from 'mode-watcher';
@@ -9,16 +8,31 @@
 </script>
 
 <header class="w-screen">
-	<span class="flex flex-row items-center pl-2 space-x-1">
-		<Button on:click={toggleMode} variant="outline" size="icon">
-			<Sun
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-			/>
-			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</Button>
+	<span class="flex flex-row items-center pl-2 space-x-2">
+		<ul class="flex flex-row space-x-2">
+			<li>
+				<Button on:click={toggleMode} variant="outline" size="icon">
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</li>
+			<li>
+				{#if $user}
+					<Button
+						on:click={async () => await logout()}
+						variant="outline"
+						data-sveltekit-preload-data="hover">Logout</Button
+					>
+				{:else}
+					<Button href="/login" variant="outline" data-sveltekit-preload-data="hover">Login</Button>
+				{/if}
+			</li>
+		</ul>
 		<Separator orientation="vertical" class="h-8" />
 		<nav class="min-h-10 items-center py-1">
 			<ul class="flex flex-row justify-end space-x-5">
@@ -26,21 +40,11 @@
 					<Button href="/" variant="ghost">Home</Button>
 				</li>
 				<li>
-					<Button href="/" variant="ghost">Chat</Button>
+					<Button href="/chat" variant="ghost">Chat</Button>
 				</li>
-				{#if $user}
-					<li>
-						<Button
-							on:click={async () => await logout()}
-							variant="ghost"
-							data-sveltekit-preload-data="hover">Logout</Button
-						>
-					</li>
-				{:else}
-					<li>
-						<Button href="/login" variant="ghost" data-sveltekit-preload-data="hover">Login</Button>
-					</li>
-				{/if}
+				<li>
+					<Button href="/profile" variant="ghost">Profile</Button>
+				</li>
 			</ul>
 		</nav>
 	</span>
