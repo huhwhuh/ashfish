7ab711c9e310ec509010fdbe78cc32fec3410692
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2024-04-25 22:08:11 -0400

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,50 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices, type PlaywrightTestConfig } from '@playwright/test';
 
+export const USER_AUTH_PATH = 'tests/.auth/user.json';
+// export const USER_AUTH_STATE = { storageState: USER_AUTH_PATH };
+export const NO_AUTH_STATE = { storageState: { cookies: [], origins: [] } };
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm run start',
+		url: 'http://localhost:5173',
+		reuseExistingServer: !process.env.CI,
+		stdout: 'ignore',
+		stderr: 'pipe'
 	},
+	use: {
+		baseURL: 'http://localhost:5173/',
+		trace: 'on-first-retry',
+		screenshot: 'only-on-failure',
+		ignoreHTTPSErrors: true
+	},
+	fullyParallel: true,
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
-};
+	projects: [
+		// Setup project
+		{
+			name: 'setup',
+			testMatch: /.*\.setup\.ts/
+		},
 
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+				storageState: 'tests/.auth/user.json'
+			},
+			dependencies: ['setup']
+		}
+		//
+		// {
+		// 	name: 'firefox',
+		// 	use: {
+		// 		...devices['Desktop Firefox'],
+		// 		// Use prepared auth state.
+		// 		storageState: USER_AUTH_PATH
+		// 	},
+		// 	dependencies: ['setup']
+		// }
+	]
+};
 export default config;
